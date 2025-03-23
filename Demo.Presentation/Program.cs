f5f91fdfc68113d8f10b5f41e4dd94d0b092fae8
Youssef Mohamed <mohmmeadkhalef22@gmail.com>
2025-03-23 16:20:25 +0200

    Project Structure

--- Demo.Presentation/Program.cs
+++ Demo.Presentation/Program.cs
@@ -21,14 +21,9 @@ namespace Demo.Presentation
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
             app.UseRouting();
-
-            app.UseAuthorization();
-
             app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
