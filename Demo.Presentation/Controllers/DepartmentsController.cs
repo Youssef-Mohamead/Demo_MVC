448e682be6d9c50da33cd64d4e341c38a14f315c
Youssef Mohamed <mohmmeadkhalef22@gmail.com>
2025-03-25 04:40:31 +0200

    Department Controller Index

---
+++ Demo.Presentation/Controllers/DepartmentsController.cs
@@ -0,0 +1,17 @@
+﻿using Demo.BusinessLogic.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing.Constraints;
+
+namespace Demo.Presentation.Controllers
+{
+    public class DepartmentsController(IDepartmentService _departmentService) : Controller
+    {
+        //BaseUrl/Departments/Index
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var departments= _departmentService.GetAllDepartments();
+            return View(departments);
+        }
+    }
+}
