1b599937f25e4f488759fbe02d7ff36643caa8da
Youssef Mohamed <mohmmeadkhalef22@gmail.com>
2025-04-01 12:01:20 +0200

    Employee Controller Create

--- Demo.Presentation/Controllers/EmployeesController.cs
+++ Demo.Presentation/Controllers/EmployeesController.cs
@@ -1,14 +1,57 @@
-﻿using Demo.BusinessLogic.Services.EmployeeServices;
+﻿using Demo.BusinessLogic.DataTransferObject.DepartmentDataTransferObject;
+using Demo.BusinessLogic.DataTransferObjects.EmployeeDataTransferObject;
+using Demo.BusinessLogic.Services.EmployeeServices;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace Demo.Presentation.Controllers
 {
-    public class EmployeesController(IEmployeeServices _employeeServices) : Controller
+    public class EmployeesController(IEmployeeServices _employeeServices, IWebHostEnvironment environment, ILogger<EmployeesController> logger) : Controller
     {
         public IActionResult Index()
         {
             var Employees = _employeeServices.GetAllEmployees();
             return View(Employees);
         }
+        #region Create Employee
+        [HttpGet]
+        public IActionResult Create() => View();
+
+        [HttpPost]
+        public IActionResult Create(CreatedEmployeeDto employeeDto)
+        {
+            if (ModelState.IsValid) // Server Side Validation
+            {
+                try
+                {
+                    int Result = _employeeServices.AddEmployee(employeeDto);
+                    if (Result > 0)
+                        return RedirectToAction(nameof(Index));
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, "Employee Can't Be Created");
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // Log Exception
+                    if (environment.IsDevelopment())
+                    {
+                        // 1. Development => Log Error In Console and Return  Same View With Error Message
+
+                        ModelState.AddModelError(string.Empty, ex.Message);
+                    }
+                    else
+                    {
+                        //2. Deployment => Log Error In File | Table in Database And Return Error View
+
+                        logger.LogError(ex.Message);
+                    }
+                }
+            }
+            return View(employeeDto);
+        }
+        #endregion
     }
 }
